cfade7af02c3f672985277d8b6a58abfd04f616a
Lino <linojr@gmail.com>
2017-06-05 15:53:26 -0500

    API results show on page.

--- resources/main.js
+++ resources/main.js
@@ -25,10 +25,29 @@ function getWikiPediaEntry(term){
     },
     dataType : "jsonp",
     crossDomain : true,
-    success: function(data){
-      console.log(data.query.search[0]);
+    success: function(json){
+      // console.log(data.query.search[0]);
+      //used to parse the data
+      showData(json);
     }
   });
+}
+
+function showData(rawData){
+  var listOfResults = rawData.query.search;
+  var title;
+  var description;
+  var link;
+  var $blankCard = '<div class="result-card"><div class="result-title"></div><div class="result-description"></div></div>';
+  for (var i = 0; i < listOfResults.length; i++ ){
+    title = listOfResults[i].title;
+    description = listOfResults[i].snippet;
+    link = "https://en.wikipedia.org/wiki/" + title;
+    console.log(title, description, link);
+    $($blankCard).addClass("item" + i).appendTo(".results-area");
+    $(".item"+ i + " .result-title" ).html("<h2>"+ title + "</h2>");
+    $(".item"+ i + " .result-description").html("<p>" + description +"</p>");
+  };
 }
 
   // show results on the pageshowresults
